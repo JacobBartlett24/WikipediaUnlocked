602a5d7de1d3b6cf3435d0c59a32ba852a1da77b
JacobBartlett24 <96956475+JacobBartlett24@users.noreply.github.com>
2022-10-10 22:36:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
-from itertools import count
 import json
 import re
 
-
-
-moviesList = []
+moviesList = [] #precursor for the list of json objects fetched through other functions
 testList = [{"title":"Manhatta","cast":['Morgan Freeman'],"directors":["[[Charles Sheeler]]","[[Paul Strand]]"],"year":1921},
 {"title":"something","cast":[],"directors":["[[Charles Sheeler]]","[[Paul Strand]]"],"year":1921},
-{"title":"some","cast":[],"directors":["[[Charles Sheeler]]","[[Paul Strand]]"],"year":1921}]
+{"title":"some","cast":[],"directors":["[[Charles Sheeler]]","[[Paul Strand]]"],"year":1921}] #test list to test functions in a smaller way
 
 with open('data.txt') as f:
     for jsonObj in f:
-        movieDict = json.loads(jsonObj)
-        moviesList.append(movieDict)
-
-def userInput():
-    actor = input("Which actor do you want to connect Kevin Bacon to? ")
-    return actor
+        movieDict = json.loads(jsonObj)  #Makes the json file "data.txt" readable for Python
+        moviesList.append(movieDict)     #Appends all json objects to the moviesList array
+        
+"""
+    NAME:       newNode
+    PURPOSE:    The newNode class contains all the innerworkings of what a node for a tree needs
+    INVARIANTS: A node has data and a pointer to the left and right node
+"""
 
 class newNode:
     def __init__(self,data):
         self.data = data
         self.left = self.right = None
 
+"""
+    NAME:           userInput
+    PARAMETERS:     None
+    PURPOSE:        To take an actor's name and return it
+    PRECONDITION:   User should run the program
+    POSTCONDITION:  Return the user's input
+"""
+        
+def userInput():
+    actor = input("Which actor do you want to connect Kevin Bacon to? ")
+    return actor
+
+"""
+    NAME:           createTree
+    PARAMETERS:     arr is an array of all movies and cast members, i is the incrementer of the tree, n is the length of the original array
+    PURPOSE:        To create a tree based on an array
+    PRECONDITION:   arr should be full of movies from the json file
+    POSTCONDITION:  return root, which should contain all of the movies sorted by order of the json file
+"""
+        
 def createTree(arr,i,n):
     root = None
 
@@ -35,6 +54,16 @@ def createTree(arr,i,n):
         root.right = createTree(arr,2 * i + 2,n)
 
     return root
+
+"""
+    NAME:           searchTree
+    PARAMETERS:     root, the entire tree of all movies. 
+                    Actor is the specified actor (Kevin Bacon and the user inputted actor).
+                    Arr is the array of movie objects
+    PURPOSE:        To search the tree for a valid actor and build an array of the movies the actor is in
+    PRECONDITION:   The root should contain the tree, actor should be the specified actor, and array should be empty
+    POSTCONDITION:  The arr should be full of all movies of actor
+"""
 
 def searchTree(root,actor,arr):
     if(root != None):
@@ -52,8 +81,8 @@ def searchTree(root,actor,arr):
     NAME:           findHeight
     PARAMETERS:     root, the tree starting at the specified movie
     PURPOSE:        To find the height of said node.
-    PRECONDITION:   The array should be full of movie nodes 
-    POSTCONDITION:  The heightArray should be full of integers representing the heights of certain movies
+    PRECONDITION:   The root should start on the specified movie of an actor
+    POSTCONDITION:  The function should return an integer of the max height between right and left
 """
 
 
